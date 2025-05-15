d31dc181f62b9a6e8f0a7fb1224e13c92b325648
brian-tura <b.tura2002@gmail.com>
2025-05-15 15:18:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={}></Route>
+          <Route path=''></Route>
+          <Route path=''></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
